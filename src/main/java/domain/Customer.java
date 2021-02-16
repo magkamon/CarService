1cabc055f9a11730762f9531ab6cf9d0c6575215
magkamon <kamonmagdalena@gmail.com>
2021-02-16 09:59:13 +0100

    customer domian class done

---
+++ src/main/java/domain/Customer.java
@@ -0,0 +1,57 @@
+package domain;
+
+import java.util.UUID;
+
+public class Customer {
+
+    private String firstname;
+    private String surname;
+    private int phone;
+    private String email;
+    private final UUID uuid;
+
+    public Customer (String firstname, String surname, int phone, String email, UUID uuid){
+        this.firstname = firstname;
+        this.surname = surname;
+        this.phone = phone;
+        this.email = email;
+        this.uuid = uuid;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname){
+        this.surname = surname;
+    }
+
+    public int getPhone() {
+        return phone;
+    }
+
+    public void setPhone(int phone){
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email){
+        this.email = email;
+    }
+
+    public UUID getUuid(){
+        return uuid;
+    }
+
+}
