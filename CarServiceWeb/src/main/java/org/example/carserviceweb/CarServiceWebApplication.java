dcc1cd47b4d27d509507165ecbc5d19adef3ee7b
magkamon <kamonmagdalena@gmail.com>
2021-03-16 17:54:10 +0100

    [CS-44] Create service, console and web modules

---
+++ CarServiceWeb/src/main/java/org/example/carserviceweb/CarServiceWebApplication.java
@@ -0,0 +1,13 @@
+package org.example.carserviceweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarServiceWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarServiceWebApplication.class, args);
+	}
+
+}
